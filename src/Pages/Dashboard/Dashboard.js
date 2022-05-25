f89b6818a651eae8fa8aa68364e8f9ce1394413f
ador <rashedulhaque111@gmail.com>
2022-05-26 03:15:37 +0600

    admin task complete

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
+
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -19,10 +25,10 @@ const Dashboard = () => {
                     <li><Link to="/dashboard/myorders">My Orders</Link></li>
                     <li><Link to="/dashboard/review">Add a review</Link></li>
                     <li><Link to="/dashboard/myprofile">My Profile</Link></li>
-                    <li><Link to="/dashboard/makeadmin">Make Admin</Link></li>
-                    <li><Link to="/dashboard/addproduct">Add Product</Link></li>
-                    <li><Link to="/dashboard/manageorders">Manage Orders</Link></li>
-                    <li><Link to="/dashboard/manageproduct">Manage Products</Link></li>
+                    {admin && <li><Link to="/dashboard/makeadmin">Make Admin</Link></li>}
+                    {admin && <li><Link to="/dashboard/addproduct">Add Product</Link></li>}
+                    {admin && <li><Link to="/dashboard/manageorders">Manage Orders</Link></li>}
+                    {admin && <li><Link to="/dashboard/manageproduct">Manage Products</Link></li>}
                 </ul>
 
             </div>
